5d898003cd1549e8788214803f4a35317daebdfd
Simonyi Gergő <28359278+gergosimonyi@users.noreply.github.com>
2022-06-29 08:45:25 +0200

    return spent_at on spending creation
    See https://github.com/rails/rails/issues/34237 as to why this isn't
    done automatically.

--- api/app/models/spending.rb
+++ api/app/models/spending.rb
@@ -2,4 +2,6 @@ class Spending < ApplicationRecord
   validates :description, presence: true, allow_blank: false
   validates :currency, presence: true, allow_blank: false
   validates :amount, presence: true, numericality: { other_than: 0 }, allow_blank: false
+
+  after_create :reload
 end
