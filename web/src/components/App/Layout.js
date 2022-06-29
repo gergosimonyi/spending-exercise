dd15c2f77b96b579aa8d299cce598d4cf4c518df
Simonyi Gergő <28359278+gergosimonyi@users.noreply.github.com>
2022-06-29 08:45:24 +0200

    organize components folder

--- web/src/components/App/Layout.js
+++ web/src/components/App/Layout.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import { FiltersAndOrderings } from "components/FiltersAndOrderings/FiltersAndOrderings";
 import { Form } from "components/Form/Form";
-import { Header } from "components/Header/Header";
 import { SpendingList } from "components/SpendingList/SpendingList";
 
-import { Main } from "./ComponentStyles";
+import { Main } from "./Main";
+import { Header } from "./Header/Header";
 
 const Layout = () => (
   <>
