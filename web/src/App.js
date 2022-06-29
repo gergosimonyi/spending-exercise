aa38d9cf21fdf8b4fd546a0afa6255ee6c434fa5
Simonyi Gergő <28359278+gergosimonyi@users.noreply.github.com>
2022-06-29 08:45:24 +0200

    remove Layout component
    Layout in the App component is legible enough

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
 import Form from "./components/Form";
 import FiltersAndOrderings from "./components/FiltersAndOrderings";
 import SpendingList from "./components/SpendingList";
-import Layout from "./components/Layout";
+import { MainContainer } from "./styles/ComponentStyles";
 
 export default function App() {
   return (
     <>
-      <Layout>
+      <Header />
+      <MainContainer>
         <Form />
         <FiltersAndOrderings />
         <SpendingList />
-      </Layout>
+      </MainContainer>
     </>
   );
 }
