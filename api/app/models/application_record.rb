5aa1606d08b3a4692d0843dc3d03e3eddeb5fd83
Simonyi Gergő <28359278+gergosimonyi@users.noreply.github.com>
2022-06-29 08:45:25 +0200

    remove metadata from API responses

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def serializable_hash(options={})
+    options[:except] ||= []
+    options[:except] << :created_at
+    options[:except] << :updated_at
+
+    super(options)
+  end
 end
