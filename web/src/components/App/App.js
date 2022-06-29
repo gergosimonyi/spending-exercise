bde389e0ccbd225ce3c1e9113643944f69740710
Simonyi Gergő <28359278+gergosimonyi@users.noreply.github.com>
2022-06-29 08:45:24 +0200

    standardize filenames in App component

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { GlobalStyle } from "./GlobalStyle";
+import { GlobalStyle } from "./App.styles";
 import { Layout } from "./Layout/Layout";
 
 const App = () => {
